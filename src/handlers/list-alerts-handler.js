67aca4646eae5c9918c0b23fa698025ca937ff9b
Vamshi K Ponnapalli <vamshi.ponnapalli@gmail.com>
2017-02-14 20:32:48 -0500

    Add additional lambda functions

---
+++ src/handlers/list-alerts-handler.js
@@ -0,0 +1,19 @@
+'use strict';
+
+/**
+ * Return a list of alerts
+ *
+ * @param {Object} event The lambda event object
+ * @param {Object} context The lambda context object
+ * @param {Function} callback A callback method to signal the completion
+ *          lambda function
+ * @param {Object} ext Extended properties containing references to injected
+ *        properties such as config, logger, etc.
+ */
+module.exports = function(event, context, callback, ext) {
+    const logger = ext.logger;
+    const config = ext.config;
+
+    //TODO: Implement function and invoke callback.
+    callback(null, 'Lambda function [list_alerts] executed successfully');
+};
